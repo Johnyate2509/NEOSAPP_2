4b5b2ebb311551cf330189285f6b09da7abbf192
maicollopezg-coder <maicol.lopezg@uniagustiniana.edu.co>
2026-05-12 14:45:49 +0000

    categprias-completas-3

--- neosapp/src/context/StoreContext.jsx
+++ neosapp/src/context/StoreContext.jsx
@@ -581,3 +581,11 @@ return (
   </StoreContext.Provider>
 );
 }
+
+export function useStore() {
+  const context = useContext(StoreContext);
+  if (!context) {
+    throw new Error('useStore must be used within StoreProvider');
+  }
+  return context;
+}
