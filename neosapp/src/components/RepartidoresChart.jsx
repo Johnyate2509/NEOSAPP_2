bad86eeff87e0c9bc331c1664d358ec11448b990
maicollopezg-coder <maicol.lopezg@uniagustiniana.edu.co>
2026-05-25 05:52:16 +0000

    FUNCIONALIDAD

--- neosapp/src/components/RepartidoresChart.jsx
+++ neosapp/src/components/RepartidoresChart.jsx
@@ -4,6 +4,7 @@ import {
   CategoryScale,
   LinearScale,
   BarElement,
+  BarController,
   Title,
   Tooltip,
   Legend,
@@ -14,6 +15,7 @@ ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
+  BarController,
   Title,
   Tooltip,
   Legend
